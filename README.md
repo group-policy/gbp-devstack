e38f13cf209d445f0a1e92a088c53e69d193c94a
Sumit Naiksatam <sumitnaiksatam@gmail.com>
2015-08-17 02:22:54 -0700

    Initial commit, adding patch script

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # gbp-devstack
-Install devstack with GBP.
+Install stable/kilo devstack with GBP.
+
+0. Install Ubuntu 14.04
+
+1. git clone https://git.openstack.org/openstack-dev/devstack -b stable/kilo
+
+2. cd devstack
+
+3. wget https://raw.githubusercontent.com/group-policy/gbp-devstack/master/gbp-patch.sh
+
+4. chmod +x gbp-patch.sh
+
+5. ./gbp-patch.sh
+
+6. ./stack.sh
+
+To verify installation run:
+
+exercises/gbp.sh
---
+++ gbp-patch.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -x
+
+wget https://raw.githubusercontent.com/group-policy/devstack/kilo-gbp/local.conf
+wget https://raw.githubusercontent.com/group-policy/devstack/kilo-gbp/lib/gbp -P lib/
+sed -i 's/source $TOP_DIR\/lib\/neutron-legacy/source $TOP_DIR\/lib\/neutron-legacy\nsource $TOP_DIR\/lib\/gbp/g' stack.sh
+sed -i 's/# Extras Configuration/install_gbpclient\ninstall_gbpservice\ninit_gbpservice\n\install_gbpheat\ninstall_gbpui\n# Extras Configuration/g' stack.sh
+sed -i 's/echo_summary "Creating initial neutron network elements"//g' stack.sh
+sed -i 's/create_neutron_initial_network//g' stack.sh
+wget https://raw.githubusercontent.com/group-policy/devstack/kilo-gbp/exercises/gbp.sh -P exercises/
+chmod +x exercises/gbp.sh
