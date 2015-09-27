4123938179ad8385db86eff1b006107967ca6809
Thomas Bachman <tbachman@yahoo.com>
2015-09-27 06:40:16 -0400

    Removed start of apache service.
    The apache start causes the socket bind to fail,
    since apache is already running.
    
    Signed-off-by: Thomas Bachman <tbachman@yahoo.com>

--- gbp-patch.sh
+++ gbp-patch.sh
@@ -4,7 +4,7 @@ set -x
 
 wget https://raw.githubusercontent.com/group-policy/devstack/sumit/test/stable/juno-gbp/local.conf
 wget https://raw.githubusercontent.com/group-policy/devstack/sumit/test/stable/juno-gbp/lib/gbp -P lib/
-sed -i 's/# Extras Configuration/source $TOP_DIR\/lib\/gbp\ninstall_gbpclient\ninstall_gbpservice\ninit_gbpservice\n\install_gbpheat\ninstall_gbpui\nsudo service apache2 restart\n# Extras Configuration/g' stack.sh
+sed -i 's/# Extras Configuration/source $TOP_DIR\/lib\/gbp\ninstall_gbpclient\ninstall_gbpservice\ninit_gbpservice\n\install_gbpheat\ninstall_gbpui\n# Extras Configuration/g' stack.sh
 sed -i 's/echo_summary "Creating initial neutron network elements"//g' stack.sh
 sed -i 's/create_neutron_initial_network//g' stack.sh
 wget https://raw.githubusercontent.com/group-policy/devstack/sumit/test/stable/juno-gbp/exercises/gbp.sh -P exercises/
