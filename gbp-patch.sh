615667658ef1241b307a18eae98e41d15a6cdf50
Sumit Naiksatam <sumitnaiksatam@gmail.com>
2016-03-28 11:58:38 -0700

    Install aim & apic_ml2 repos

--- gbp-patch.sh
+++ gbp-patch.sh
@@ -5,7 +5,7 @@ set -x
 wget https://raw.githubusercontent.com/group-policy/gbp-devstack/master/local.conf
 wget https://raw.githubusercontent.com/group-policy/gbp-devstack/master/lib/gbp -P lib/
 sed -i 's/source $TOP_DIR\/lib\/neutron-legacy/source $TOP_DIR\/lib\/neutron-legacy\nsource $TOP_DIR\/lib\/gbp/g' stack.sh
-sed -i 's/# Extras Configuration/install_gbpclient\ninstall_gbpservice\ninit_gbpservice\n\install_gbpheat\ninstall_gbpui\nsudo service apache2 restart\n# Extras Configuration/g' stack.sh
+sed -i 's/# Extras Configuration/\ninstall_apic_ml2\ninstall_aim\ninit_aim\ninstall_gbpclient\ninstall_gbpservice\ninit_gbpservice\n\install_gbpheat\ninstall_gbpui\nsudo service apache2 restart\n# Extras Configuration/g' stack.sh
 sed -i 's/echo_summary "Creating initial neutron network elements"//g' stack.sh
 sed -i 's/create_neutron_initial_network//g' stack.sh
 wget https://raw.githubusercontent.com/group-policy/gbp-devstack/master/exercises/gbp.sh -P exercises/
